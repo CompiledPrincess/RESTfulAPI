5710f047079f4d73ab73ff4aa3a98d63022ea011
Qing Jin <qingjinfullstack@gmail.com>
2023-04-01 16:47:20 -0500

    test02

--- calendar/src/main/java/qing/jin/calendar/Controller/ContentController.java
+++ calendar/src/main/java/qing/jin/calendar/Controller/ContentController.java
@@ -1,9 +1,39 @@
 package qing.jin.calendar.Controller;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import qing.jin.calendar.Model.Content;
+import qing.jin.calendar.Model.Status;
+import qing.jin.calendar.Model.Type;
+import qing.jin.calendar.Repo.ContentCollectionRepo;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/content")
+@RequestMapping("/api/content") //localhost:8080/api/content
 public class ContentController {
+    private final ContentCollectionRepo contentCollectionRepo;
+    @Autowired
+    public ContentController(ContentCollectionRepo contentCollectionRepo) {
+        this.contentCollectionRepo = contentCollectionRepo;
+    }
+
+    @GetMapping("")
+    public List<Content> findAll() {
+        return contentCollectionRepo.findall();
+    }
+
+    @GetMapping("{id}")
+    public Content findById(@PathVariable Integer id) {
+        return contentCollectionRepo.findById(id).
+                orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND,"Resources Not Found"));
+    }
 }
--- calendar/src/main/java/qing/jin/calendar/Repo/ContentCollectionRepo.java
+++ calendar/src/main/java/qing/jin/calendar/Repo/ContentCollectionRepo.java
@@ -1,8 +1,12 @@
 package qing.jin.calendar.Repo;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Repository;
 import qing.jin.calendar.Model.Content;
+import qing.jin.calendar.Model.Status;
+import qing.jin.calendar.Model.Type;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -20,5 +24,20 @@ public class ContentCollectionRepo {
 
     public Optional<Content> findById(Integer id) {
         return content.stream().filter(c -> c.id().equals(id)).findFirst();
+    }
+
+    @PostConstruct
+    private void init() {
+        Content c = new Content(
+                1,
+                "Qing",
+                "Qing testing",
+                Status.IN_PROGRESS,
+                Type.COURSE,
+                LocalDateTime.now(),
+                null, "https://github.com/CompiledPrincess"
+        );
+        content.add(c);
+
     }
 }
